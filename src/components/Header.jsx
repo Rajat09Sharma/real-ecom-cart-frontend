04cf207a8d57182e87b3b6a67a0b356023f3947d
Rajat Sharma <rajat9.lyfe@gmail.com>
2025-10-30 13:32:02 +0530

    real-ecom-cart-frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export const Header = () => {
             <header className="w-full flex justify-between items-center py-4 px-6 mb-4">
                 <div className="md:w-2/5 flex items-center gap-3 md:gap-5">
                     <img src="/logo.png" alt="Elegant model" className="w-10 md:w-1/12" />
-                    <h1 className="text-2xl text-yellow-400 font-bold md:text-4xl">Elegant</h1>
+                    <h1 className="text-2xl text-yellow-400 font-bold md:text-4xl">Vibe Commerce</h1>
                 </div>
                 <div>
                     <button disabled={!notify ? false : true} type="button" className="py-1 px-2 md:py-2 md:px-3 bg-yellow-300 text-neutral-700 font-semibold text-lg md:text-xl border-0 rounded-md cursor-pointer transition-colors duration-150 hover:bg-yellow-400" onClick={handleModalOpen} >Cart {cartItems.length > 0 && `(${cartItems.length})`}</button>
